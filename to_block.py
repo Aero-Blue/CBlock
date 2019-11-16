8b39d4d15688e3687ca4c43486035954fd7cd38b
Aero Blue <48103037+Aero-Blue@users.noreply.github.com>
2019-11-16 15:35:49 -0600

    Add class

--- to_block.py
+++ to_block.py
@@ -1,28 +1,30 @@
 import sys
 
 
-def from_file(filepath):  # Get a list of stripped lines from a text file
-    return [line.rstrip("\n") for line in open(filepath)]
+class BlockComment:
+    def __init__(self, in_file, out_file):
+        self.in_file = in_file
+        self.out_file = out_file
+        self.lines = self.from_file()
+        self.line_length = self.find_longest()
+        self.comment_block = self.make_block()
 
+    def from_file(self):  # Get a list of stripped lines from a text file
+        return [line.rstrip("\n") for line in open(self.in_file)]
 
-def find_longest(lines):  # Find longest string in a list of strings
-    return max([len(line) for line in lines])
+    def find_longest(self):  # Find longest string in a list of strings
+        return max([len(line) for line in self.lines])
 
+    def comment(self, line):  # Comment line
+        return f"/*   {line}{' ' * (self.line_length - len(line))}   */"
 
-def c_line(line):  # Comment line
-    return f"/*   {line}{' '*(longest-len(line))}   */"
+    def make_block(self):
+        edge = f"/{'*' * (self.line_length + 8)}/"  # Top / bottom of comment block
+        return "\n".join([edge] + list(map(self.comment, self.lines)) + [edge])  # Concatenate block
 
-
-def c_block(lines):
-    global longest  # Length of longest line in list
-    longest = find_longest(lines)
-    edge = f"/{'*'*(longest+8)}/"  # Top / bottom of comment block
-    return [edge] + list(map(c_line, lines)) + [edge]  # Concatenate block
-
-
-def to_file(filepath, text):  # Output text to file
-    return open(filepath, "w").write("\n".join(text))
+    def to_file(self):  # Output text to file
+        open(self.out_file, "w").write(self.comment_block)
 
 
 if __name__ == '__main__':
-    to_file(sys.argv[2], c_block(from_file(sys.argv[1])))
+    BlockComment(sys.argv[1], sys.argv[2]).to_file()
