7c7412478270c1f7c79826f5bcf4b54839cfb670
Aero Blue <48103037+Aero-Blue@users.noreply.github.com>
2019-11-17 23:11:14 -0600

    Add class

--- to_block.py
+++ to_block.py
@@ -2,18 +2,19 @@ import sys
 
 
 class BlockComment:
+    __slots__ = ['in_file', 'out_file', 'lines', 'line_length', 'comment_block']
+
     def __init__(self, in_file, out_file):
         self.in_file = in_file
         self.out_file = out_file
         self.lines = self.from_file()
         self.line_length = self.find_longest()
-        self.comment_block = self.make_block()
 
     def from_file(self):  # Get a list of stripped lines from a text file
-        return [line.rstrip("\n") for line in open(self.in_file)]
+        return list(map(lambda x: x.rstrip("\n"), open(self.in_file)))
 
     def find_longest(self):  # Find longest string in a list of strings
-        return max([len(line) for line in self.lines])
+        return max(map(lambda x: len(x), self.lines))
 
     def comment(self, line):  # Comment line
         return f"/*   {line}{' ' * (self.line_length - len(line))}   */"
@@ -22,9 +23,13 @@ class BlockComment:
         edge = f"/{'*' * (self.line_length + 8)}/"  # Top / bottom of comment block
         return "\n".join([edge] + list(map(self.comment, self.lines)) + [edge])  # Concatenate block
 
-    def to_file(self):  # Output text to file
-        open(self.out_file, "w").write(self.comment_block)
+    def export(self):  # Output text to file
+        open(self.out_file, "w").write(str(self))
+        print(f"Exported {len(self.lines)+2} lines to '{self.out_file}'")
+
+    def __str__(self):
+        return self.make_block()
 
 
 if __name__ == '__main__':
-    BlockComment(sys.argv[1], sys.argv[2]).to_file()
+    BlockComment(sys.argv[1], sys.argv[2]).export()
